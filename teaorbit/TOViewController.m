facade16ab65bc1391ee98b3be5003f33cfec76c
Murat Ayfer <murat@ayfer.net>
2013-12-14 17:44:17 -0800

    webview javascript bridge works!!!

--- teaorbit/TOViewController.m
+++ teaorbit/TOViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "TOViewController.h"
+#import "WebViewJavascriptBridge.h"
 
-@interface TOViewController ()
+@interface TOViewController () {
+    WebViewJavascriptBridge* bridge;
+}
 @property (strong, nonatomic) IBOutlet UIWebView *TOWebView;
 
 @end
@@ -18,10 +21,28 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	NSString *fullURL = @"http://teaorbit.com";
+	NSString *fullURL = @"http://localhost:8001/dev01";
     NSURL *url = [NSURL URLWithString:fullURL];
     NSURLRequest *requestObj = [NSURLRequest requestWithURL:url];
-    [_TOWebView loadRequest:requestObj];}
+    [_TOWebView loadRequest:requestObj];
+    
+    bridge = [WebViewJavascriptBridge bridgeForWebView:_TOWebView handler:^(id data, WVJBResponseCallback responseCallback) {
+        NSLog(@"Received message from javascript: %@", data);
+        responseCallback(@"Right back atcha");
+    }];
+}
+
+- (void)webViewDidFinishLoad:(UIWebView *)webView
+{
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
+    
+    NSLog(@"Attempting to send shiet");
+    [bridge send:@"Well hello there"];
+    [bridge send:[NSDictionary dictionaryWithObject:@"Foo" forKey:@"Bar"]];
+    [bridge send:@"Give me a response, will you?" responseCallback:^(id responseData) {
+        NSLog(@"ObjC got its response! %@", responseData);
+    }];
+}
 
 - (void)didReceiveMemoryWarning
 {
